bbd2b95afd74768d9876baf9010990e62fcd3db3
Mateus Cavalcante <mateuscvte@gmail.com>
2020-10-31 14:21:53 -0300

    front-login

---
+++ src/app/theme/theme.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {LoginModule} from "./login/login.module";
+import {MatButtonModule} from '@angular/material/button';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LoginModule,
+
+  ]
+})
+export class ThemeModule { }
